cffac0ec4771dcc7bfdc07659ded3ef7b4f4ca11
Aaron Suszka <aaron.suszka@gmail.com>
2022-12-22 09:13:58 +0100

    S1 - Dreieck mit 3 verschiedenen Seiten und Flaechenberechnung erstellt

---
+++ RefactoringAufgabe/RefactoringAufgabe/Dreieck.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactoringAufgabe
+{
+    internal class Dreieck
+    {
+        private double seitenlaenge1;
+        private double seitenlaenge2;
+        private double seitenlaenge3;
+        private double winkel1;
+        private double winkel2;
+
+        public Dreieck(double seitenlaenge1, double seitenlaenge2, double seitenlaenge3)
+        {
+            this.seitenlaenge1 = seitenlaenge1;
+            this.seitenlaenge2 = seitenlaenge2;
+            this.seitenlaenge3 = seitenlaenge3;
+        }
+
+        public double Fläche
+        {
+            get
+            {
+                double temp = (seitenlaenge1 + seitenlaenge2 + seitenlaenge3) / 2;
+                return Math.Sqrt(temp * (temp - seitenlaenge1) * (temp - seitenlaenge2) * (temp - seitenlaenge3));
+            }
+        }
+
+        public double Seitenlaenge1
+        {
+            get => seitenlaenge1;
+        }
+
+        public double Seitenlaenge2
+        {
+            get => seitenlaenge2;
+        }
+
+        public double Seitenlaenge3
+        {
+            get => seitenlaenge3;
+        }
+
+    }
+}
---
+++ RefactoringAufgabe/RefactoringAufgabe/Program.cs
@@ -0,0 +1,25 @@
+﻿using RefactoringAufgabe;
+
+double a;
+double b;
+double c;
+
+Console.Write("Seitenlänge1: ");
+a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Seitenlänge2: ");
+b = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Seitenlänge3: ");
+c = Convert.ToDouble(Console.ReadLine());
+
+Dreieck dreieck1 = new Dreieck(a, b, c);
+
+Console.WriteLine(dreieck1.Fläche);
+
+
+
+
+
+
+
